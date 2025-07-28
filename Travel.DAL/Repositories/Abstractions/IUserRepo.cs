2c4581b87e6c37e80eb5c0ff6e3138f8c1299ee2
Sara Ramadan <sara.ramadan@garment.io>
2025-07-28 19:09:53 +0300

    add repo

---
+++ Travel.DAL/Repositories/Abstractions/IUserRepo.cs
@@ -0,0 +1,11 @@
+﻿using Travel.DAL.Entities.Models;
+
+namespace Travel.DAL.Repositories.Abstractions
+{
+    public interface IUserRepo
+    {
+        //i want GetById to find the name of user
+        public string GetUserById(int id);
+
+    }
+}
---
+++ Travel.DAL/Repositories/Implementations/LikeRepo.cs
@@ -0,0 +1,51 @@
+﻿using Travel.DAL.DataBase;
+using Travel.DAL.Entities.Models;
+using Travel.DAL.Repositories.Abstractions;
+
+namespace Travel.DAL.Repositories.Implementations
+{
+    public class LikeRepo : ILikeRepo
+    {
+        private readonly TravelStateDbContext db;
+        public LikeRepo(TravelStateDbContext db)
+        {
+            this.db = db;
+        }
+        public (bool, string?) CreateLike(Like like)
+        {
+            try
+            {
+                db.likes.Add(like);
+                db.SaveChanges();
+                return (true, null);
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
+        public (bool, string?) DeleteLike(int id)
+        {
+            try
+            {
+                var like = db.likes.Where(a => a.PostId == id).FirstOrDefault();
+                if (like == null)
+                    return (false, "error no found ");
+                like.DeleteLike();
+                db.SaveChanges();
+                return (true, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
+        public List<Like> GetAllLikes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
